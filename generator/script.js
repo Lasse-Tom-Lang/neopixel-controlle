433c4f8cd92560fb0f3c3c2e62f1b38eb02a8a5a
Lasse Lang <lang.lasse@gmx.de>
2023-06-12 10:06:25 +0200

    Added animation timeline

--- generator/script.js
+++ generator/script.js
@@ -1,7 +1,17 @@
 const checkboxes = document.querySelectorAll("input[type='checkbox']")
 const output = document.getElementById("output")
+const framesDiv = document.getElementById("frames")
+const clearButton = document.getElementById("clearButton")
 
-function generateList() {
+let frames = []
+
+clearButton.addEventListener("click", () => {
+  checkboxes.forEach(checkbox => {
+    checkbox.checked = false
+  })
+})
+
+function addFrame() {
   let string = "{"
   let first = true
   checkboxes.forEach(checkbox => {
@@ -19,6 +29,31 @@ function generateList() {
     }
   })
   string += "}"
-  output.innerText = string
+  frames.push({number: frames.length + 1, text: string})
+  showFrames()
+}
+
+function showFrames() {
+  framesDiv.innerHTML = ""
+  frames.forEach(frame => {
+    let frameButton = document.createElement("button")
+    frameButton.innerText = frame.number
+    frameButton.addEventListener("click", loadFrame)
+    frameButton.setAttribute("frameData", frame.text)
+    framesDiv.appendChild(frameButton)
+  })
+}
+
+function loadFrame(event) {
+  frameInfo = event.target.getAttribute("frameData")
+  frameInfo = frameInfo.substring(1, 74)
+  for (let i = 0; i<37; i++) {
+    if (frameInfo[i*2] == "0") {
+      checkboxes[i].checked = false
+    }
+    else {
+      checkboxes[i].checked = true
+    }
+  }
 }
 
